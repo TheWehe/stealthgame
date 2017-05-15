e4317985db293220471e35c73f52f68c1a5487c9
TheWehe <luca_wellmeier@gmx.de>
2017-05-16 01:40:01 +0200

    blub

--- src/stealthgame/Main.java
+++ src/stealthgame/Main.java
@@ -1,10 +1,44 @@
 package stealthgame;
 
-public class Main {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("luca kann gar nichts");
+public class Main extends BasicGame
+{
+	public Main(String gamename)
+	{
+		super(gamename);
 	}
 
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Main("Simple Slick Game"));
+			appgc.setDisplayMode(1920, 1080, true);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
