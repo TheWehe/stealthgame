1f5b07b0478757cd6f3d5325be4be3c5f57eacdc
LordKampfdattel <TheGameIng123@web.de>
2017-05-19 20:35:28 +0200

    Menu

--- src/stealthgame/GameState_GameMenu.java
+++ src/stealthgame/GameState_GameMenu.java
@@ -4,6 +4,8 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -13,6 +15,8 @@ public class GameState_GameMenu extends BasicGameState {
 	private Main game;
 	
 	private Input input;
+	
+	private Menu menu;
 	
 
 	public GameState_GameMenu(Main game) {
@@ -24,21 +28,23 @@ public class GameState_GameMenu extends BasicGameState {
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException
 	{
+		input = gc.getInput();
 		
+		this.menu = new Menu();
+		this.menu.addPart(new Button("Button", new Vector2f(500, 500), new Rectangle(500, 500, 50, 50)));
 	}
 	
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-		
+		this.menu.render(g);
 	}
 	
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int arg2) throws SlickException
 	{
-		input = gc.getInput();
-		
-		if(input.isKeyDown(Input.KEY_R)) this.game.enterState(0);
+		Button mb = (Button)this.menu.getPart("Button");
+		if(mb.isPressed(gc.getInput())) this.game.enterState(0);
 	}
 	
 	@Override
