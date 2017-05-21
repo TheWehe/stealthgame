51db94ba8de1abc45af933914bf5a5ce710603c1
TheWehe <luca_wellmeier@gmx.de>
2017-05-21 02:40:33 +0200

    nachtrag

--- src/stealthgame/GameState_MainMenu.java
+++ src/stealthgame/GameState_MainMenu.java
@@ -3,7 +3,6 @@ package stealthgame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
